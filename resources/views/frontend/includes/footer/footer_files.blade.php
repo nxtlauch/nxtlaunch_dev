a72b9b410a42e257f7b2ee51f6c36818ba53e528
nxtlauch <nxtlauch@gmail.com>
2017-12-18 12:31:12 +0600

    hash tag search change

--- resources/views/frontend/includes/footer/footer_files.blade.php
+++ resources/views/frontend/includes/footer/footer_files.blade.php
@@ -42,19 +42,26 @@
         // Declare variables
         var input, filter, ul, li, a, i;
         input = document.getElementById('myInput');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myUL");
-        li = ul.getElementsByTagName('li');
+        var res = input.value.charAt(0);
+        if(res=='#'){
+            $("#myUL").addClass('hidden');
+        }else {
+            $("#myUL").removeClass('hidden');
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("myUL");
+            li = ul.getElementsByTagName('li');
 
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("a")[0];
-            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
+            // Loop through all list items, and hide those who don't match the search query
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
             }
         }
+
     }
 
     function myChatFunction() {
